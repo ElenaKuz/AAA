2e1817b1e10ed50e9c99247dd323984de803ad12
ElenaKuz <kuzmenkoelena03@mail.ru>
2018-07-04 12:35:28 +0600

    Factory Tests

---
+++ MyCalculatorTests/OneArgumentTests/OneArgumentFactoryTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using WindowsFormsApp1.OneArgument;
+using NUnit.Framework;
+
+namespace MyCalculatorTests
+{
+    class OneArgumentFactoryTests
+    {
+        [TestCase("Sin", typeof(SinCalculator))]
+        [TestCase("Cos", typeof(CosCalculator))]
+        [TestCase("Pow", typeof(PowCalculator))]
+        [TestCase("Log10", typeof(Log10Calculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = OneArgumentFactory.createCalculate(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
---
+++ MyCalculatorTests/TwoArgumentTests/TwoArgumentsFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using WindowsFormsApp1.TwoArguments;
+using NUnit.Framework;
+
+namespace MyCalculatorTests.TwoArgumentTests
+{
+    public class TwoArgumentsFactoryTests
+    {
+
+        [TestCase("Add", typeof(PlusCalculator))]
+        [TestCase("Substract", typeof(MinusCalculator))]
+        [TestCase("Multiply", typeof(MultipyCalculator))]
+        [TestCase("Divide", typeof(DivisionCalculator))]
+        public void CalculateTest(string name, Type type)
+        {
+            var calculator = TwoArgumentFactory.createCalculate(name);
+
+            Assert.IsInstanceOf(type, calculator);
+        }
+
+    }
+}
